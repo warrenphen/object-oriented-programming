afbb392ab704879cf9053e738c59a7ea294b0293
Warren Phen <warren.phen@gmail.com>
2015-04-02 15:47:39 -0400

    created parent class: person.  Created instances and called methods.

--- exercise1.rb
+++ exercise1.rb
@@ -1,12 +1,41 @@
-class Student
+class Person
+	attr_accessor :name 
+	def initialize(name)
+		@name = name
+	end
+
+	def greeting
+		puts "Hi, my name is #{@name}" 
+	end
+end
+
+class Student < Person
 	def learn
 		puts "I get it!"
 	end
 end
 
-class Instructor
+class Instructor < Person
 	def teach
 		puts "Everything in Ruby is an Object"
 	end
 end
+
+
+chris = Instructor.new("Chris")
+chris.greeting
+
+cristina = Student.new("Cristina")
+cristina.greeting
+
+chris.teach
+
+cristina.learn
+
+#cristina.teach 
+
+# The .teach method only applies to instructor objects
+
+
+
 
